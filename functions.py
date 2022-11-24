b4bd856615ac40ce3a74d35240677e6e5df9947b
MilanSiuu <gabriel.milan@students.jedlik.eu>
2022-11-24 12:37:36 +0100

    menu elkészítése

--- functions.py
+++ functions.py
@@ -1,3 +1,16 @@
 from data import autókéstulajdonosai
 
 fajl='autók és tulajdonosai.txt'
+
+def menu():
+    system='cls'
+    valasztott=''
+    print('------------MENÜ------------')
+    print('0 - Kilépés')
+    print('1 - Összes autó kiírása')
+    print('2 - Egy adott autó kiírása tulajdonossal')
+    print('3 - Új parkoló autó')
+    print('4 - Autó törlése')
+    print('5 - Összes hely száma')
+    print('6 - Szabad helyek száma')
+    print('7 - Összes parkoló autó kiírása tulajdonossal')
