d3ab3ab6a2f5859ba327352f335d153f3d2673eb
MilanSiuu <gabriel.milan@students.jedlik.eu>
2022-11-24 12:47:40 +0100

    menusor

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 from data import autókéstulajdonosai
 from os import system
+from functions import menu
+
+valasztott=''
+while valasztott!='0': 
+    system('cls')
+    valasztott=menu()
+    if valasztott=='1':
+        pass
+    elif valasztott=='2':
+        pass
+    elif valasztott=='3':
+        pass
+    elif valasztott=='4':
+        pass
+    elif valasztott=='5':
+        pass
+    elif valasztott=='6':
+        pass
+    elif valasztott=='7':
+        pass
+        
